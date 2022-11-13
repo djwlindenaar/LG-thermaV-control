3bbba2c191f5a4d8bb443355f4d288ae9efb0608
Daniel Lindenaar <daniel-git@lindenaar.eu>
2022-11-13 14:34:06 +0100

    Move pump control to state machine (Afterrun to be implemented)

--- state_machine.c
+++ state_machine.c
@@ -1,7 +1,7 @@
           lambda: !lambda |-
             static const char*state_string[] =
-                       {"Idle", "Starting", "EarlyRun", "Running", "Stopping"};
-            enum States {Idle ,  Starting ,  EarlyRun ,  Running ,  Stopping};
+                       {"Idle", "Starting", "EarlyRun", "Running", "Stopping", "Afterrun"};
+            enum States {Idle ,  Starting ,  EarlyRun ,  Running ,  Stopping ,  Afterrun};
             static States state = Idle;
             auto water_temp_call = id(water_temp_target_output).make_call();
             
@@ -9,6 +9,15 @@
               case Idle:
                 if (id(thermostat_wp_heat).state) {
                   state = Starting;
+                  id(modbus_enable_heat).turn_on();
+                }
+                // when Idle, we run the pump to circulate warm water, if that's useful
+                if ((id(huiskamer_vloer).state<20.0) || // vloer is koud, rondpompen heeft weinig zin
+                    (id(huiskamer_lucht).state>id(huiskamer_vloer).state)) { //lucht is warmer dan de vloer, rondpompen heeft weinig zin
+                  ESP_LOGD("modbus_enable_heat", "Turned controller off: %f %f", id(huiskamer_vloer).state, id(huiskamer_lucht).state);
+                  id(modbus_enable_heat).turn_off();
+                } else {
+                  ESP_LOGD("modbus_enable_heat", "Turned controller on: %f %f", id(huiskamer_vloer).state, id(huiskamer_lucht).state);
                   id(modbus_enable_heat).turn_on();
                 }
                 break;
