28a18ae6956c56f27e647b0c94c31885b75ea2e6
marcokemp <77441991+marcokemp@users.noreply.github.com>
2022-12-30 22:06:09 +0100

    Update state_machine.c
    - [line 2] formatting to satisfy my editor
    - [line 18] fixed start at >min_wtemp vs fixed value 20
    - [line 88] added base_stooklijn_flow to customizations to allow stooklijn input at a configurable flow
    - [line 101] when stopping set to min_wtemp vs hardcoded 20.

--- packages/state_machine.c
+++ packages/state_machine.c
@@ -1,6 +1,5 @@
           lambda: !lambda |-
-            static const char*state_string[] =
-                       {"Idle", "Starting", "EarlyRun", "Running", "Defrosting", "Stopping", "Afterrun"};
+            static const char*state_string[] = {"Idle", "Starting", "EarlyRun", "Running", "Defrosting", "Stopping", "Afterrun"};
             enum States {Idle ,  Starting ,  EarlyRun ,  Running ,  Defrosting ,  Stopping ,  Afterrun};
             static States state = Idle;
             static States newstate = Idle;
@@ -16,7 +15,7 @@
               if (id(compressor_running).state) {
                 newstate = state = Running;
               } else {
-                if (id(water_temp_target_output).state > 20)
+                if (id(water_temp_target_output).state > (id(stooklijn_min_wtemp).state))
                   newstate = state = Starting;
                 else
                   newstate = state = Idle;
@@ -86,7 +85,7 @@
                   // concept it that we correct the delta between stooklijn target and returning water temp by the flow rate. My stooklijn
                   // is more or less accurate for the minimal flow rate of 17.5 lpm, so that's the baseline flow. (i.e. the stooklijn
                   // temperatures are correct at 17.5 lpm, e.g. at 35 lpm, the delta is halved.
-                  double corrected_stooklijn = (id(stooklijn_target) - id(water_temp_retour).state) * 17.5 / id(current_flow_rate).state + id(water_temp_retour).state;
+                  double corrected_stooklijn = (id(stooklijn_target) - id(water_temp_retour).state) * (C->base_stooklijn_flow) / id(current_flow_rate).state + id(water_temp_retour).state;
 
                   double target = corrected_stooklijn + clamp((double)(id(thermostat_error).state * id(thermostat_error_gain).state), C->max_stooklijn_correction_neg, C->max_stooklijn_correction_pos);
                   double delta = id(water_temp_aanvoer).state - target;
@@ -99,7 +98,7 @@
                     id(modbus_enable_heat).turn_off();
                     id(force_run_end).turn_off();
                     id(thermostat_wp_heat).turn_off();
-                    set_target_temp(20.0);
+                    set_target_temp(id(stooklijn_min_wtemp).state);
                     newstate = Stopping;
                     break;
                   }
